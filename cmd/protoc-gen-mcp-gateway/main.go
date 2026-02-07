8744fb0a528b93f75ab7bae189ed76a1efb05229
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2026-02-07 03:50:35 +0100

    fix: typo on proto3 declaration

--- cmd/protoc-gen-mcp-gateway/main.go
+++ cmd/protoc-gen-mcp-gateway/main.go
@@ -7,12 +7,13 @@ import (
 	"grpc-mcp-gateway/internal/annotations"
 
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
 	opts := protogen.Options{}
 	opts.Run(func(plugin *protogen.Plugin) error {
-		plugin.SupportedFeatures = uint64(protogen.FeatureProto3Optional)
+		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, file := range plugin.Files {
 			if !file.Generate {
 				continue
